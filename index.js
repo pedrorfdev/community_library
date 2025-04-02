8964585763335b9811120cf626c456be98fde8c4
Pedro Ferreira <pedrorf.dev@gmail.com>
2025-04-02 11:41:33 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from 'express'
+const app = express()
+
+app.use(express.json())
+
+const users = []
+app.post("/users", (req, res) => {
+    const body = req.body
+
+    users.push(body)
+    res.status(201).send("Usuário criado com sucesso!")
+})
+
+app.get("/users", (req, res) => {
+    res.send({users})
+})
+
+app.listen(3000, () => console.log("Server running on port 3000"))
